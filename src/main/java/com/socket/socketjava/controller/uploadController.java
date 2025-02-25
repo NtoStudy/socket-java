efeeaac3d0253d1cc9c2fbe3f735e939992b1432
NtoStudy <2943343011@qq.com>
2025-02-25 18:02:11 +0800

    新增文件上传

--- src/main/java/com/socket/socketjava/controller/uploadController.java
+++ src/main/java/com/socket/socketjava/controller/uploadController.java
@@ -1,4 +1,38 @@
 package com.socket.socketjava.controller;
 
+import com.socket.socketjava.result.Result;
+import com.socket.socketjava.utils.AliyunOssOperator;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@Tag(name="上传文件")
 public class uploadController {
+    @Autowired
+    private AliyunOssOperator aliyunOssOperator;
+
+    @PostMapping("/upload")
+    public Result uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            // 将上传的文件转换为 File 对象
+            File fileToUpload = convertMultiPartToFile(file);
+
+            // 调用工具类上传文件
+            return Result.ok(aliyunOssOperator.uploadFile(fileToUpload));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail()    ;
+        }
+    }
+
+    private File convertMultiPartToFile(MultipartFile multipartFile) throws IOException {
+        return new File(multipartFile.getOriginalFilename());
+    }
 }
--- src/main/java/com/socket/socketjava/utils/AliyunOssOperator.java
+++ src/main/java/com/socket/socketjava/utils/AliyunOssOperator.java
@@ -1,4 +1,49 @@
 package com.socket.socketjava.utils;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Component
 public class AliyunOssOperator {
+    @Value("${aliyun.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${aliyun.oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aliyun.oss.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${aliyun.oss.bucketName}")
+    private String bucketName;
+
+    public String uploadFile(File file) throws IOException {
+        // 创建 OSS 客户端
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            // 读取文件内容
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+
+            // 上传文件
+            String objectName = file.getName();
+            ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(fileContent));
+
+            // 返回文件访问 URL
+            return "https://" + bucketName + "." + endpoint + "/" + objectName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            // 关闭 OSS 客户端
+            ossClient.shutdown();
+        }
+    }
 }
